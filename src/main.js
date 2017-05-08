64aa8da545b8eea6d741e9e84cf62fd9be5d0d86
784757555@qq.com <784757555@qq.com>
2017-05-08 17:34:05 +0800

    main

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './main.css'
+
+export default class main  {
+	render(){
+		return(
+			<header>
+				<nav class="Title-Set">
+				<i class="Set-like iconfont">&#xe699;</i>
+				<i class="Set-user iconfont">&#xe630;</i>
+				<i class="Set-vidio iconfont">&#xe6be;</i>
+				</nav>
+			</header>
+		)
+	}
+}
